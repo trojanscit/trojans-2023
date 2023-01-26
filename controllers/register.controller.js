be964c889536efe30a3cc99e7cb1d88cc33eaf66
Prasanna <prasannavelmurugan0200@gmail.com>
2023-01-26 20:25:34 +0530

    rv

---
+++ controllers/register.controller.js
@@ -0,0 +1,3 @@
+exports.registerForm = async (req, res) => {
+	res.send("Hello World");
+};
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
-
 const app = express();
+const db = require("./database");
+const register = require("./routes/register");
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
-});
+app.use("/", register);
 
-app.listen(8080, () => {
-	console.log("Server is running in 8080");
+app.listen(process.env.PORT, () => {
+	console.log(`Server is running in ${process.env.PORT}`);
 });
