0274082d1cd74da89b7a9c1b6bcf6e5d10e92a97
Prasanna <prasannavelmurugan0200@gmail.com>
2023-01-26 20:25:05 +0530

    rv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Events from "./Pages/Events";
+import Home from "./Pages/Home";
+import Registration from "./Pages/Registration";
+import Sponsors from "./Pages/Sponsors";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/registration" element={<Registration />}></Route>
+				<Route path="/sponsors" element={<Sponsors />}></Route>
+				<Route path="/events" element={<Events />}></Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
