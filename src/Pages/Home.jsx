c97ee805680b9adf5bce03859e383801d56fe20d
Prasanna <prasannavelmurugan0200@gmail.com>
2023-02-02 19:13:11 +0530

    rv

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,44 +1,13 @@
 import React from "react";
-import "../styles/home.css";
-import scrol from "../images/scroll.png";
-import NavBar from "../Pages/Navbar";
+import Sponsors from "./Sponsors";
+import Hero from "../Components/Home/Hero";
 function Home() {
-  return (
-    <div className="container">
-      <NavBar />
-      <div className="dept">
-        <h1 id="intro">Department of Infomation Technology Presents</h1>
-        {/* <h1 id="quotes">hekl alf fadf af ghs v al fasfas as fksffsdfs asfaf</h1> */}
-      </div>
-      <div className="reg">
-        <button>
-          <span class="effect"></span>
-          <span class="effect"></span>
-          <span class="effect"></span>
-          <span class="text">register</span>
-        </button>
-      </div>
-      <div className="scroll">
-        <h2>scroll down</h2>
-        <img src={scrol}></img>
-      </div>
-
-      <div class="area">
-        <ul class="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<Hero></Hero>
+			<Sponsors></Sponsors>
+		</>
+	);
 }
 
 export default Home;
