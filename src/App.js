9c3eab70b1a6f3cc3f79338654be462facf8badd
Prasanna <prasannavelmurugan0200@gmail.com>
2023-02-20 08:14:31 +0530

    rv

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import EventsDetails from "./Components/Events/EventsDetails";
 import Events from "./Components/Events/Events";
 import Homepage from "./Pages/home/aboutus";
@@ -12,30 +12,44 @@ import Navbar from "./Pages/navbar/navbar";
 import Demo2 from "./Pages/workshop/Demo2";
 
 function App() {
-  return (
-    <div>
-      {" "}
-      {/* <Navbar /> */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/sponsors" element={<Sponsors />}></Route>
-          <Route path="/registration" element={<Registration />}></Route>
-          <Route path="/events" element={<Events />}></Route>
-          <Route path="/teams" element={<Teams />}></Route>
-          <Route path="/workshop" element={<Demo2 />}></Route>
-          <Route path="/events/:eventType" element={<EventsDetails />}></Route>
-        </Routes>
-      </Router>
-    </div>
+	return (
+		<div>
+			{" "}
+			{/* <Navbar /> */}
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/sponsors" element={<Sponsors />}></Route>
+					<Route
+						path="/registration"
+						element={<Registration />}
+					></Route>
+					<Route path="/events" element={<Events />}></Route>
+					<Route path="/teams" element={<Teams />}></Route>
+					<Route path="/workshop" element={<Demo2 />}></Route>
+					<Route
+						path="/events/technical"
+						element={<EventsDetails />}
+					></Route>
+					<Route
+						path="/events/non-technical"
+						element={<EventsDetails />}
+					></Route>
+					<Route
+						path="/events/gaming"
+						element={<EventsDetails />}
+					></Route>
+				</Routes>
+			</Router>
+		</div>
 
-    // <div className="body">
-    //   {/* <Events /> */}
-    //   <Homepage />
-    //   {/* <Sponsors /> */}
-    //   {/* <Workshop /> */}
-    // </div>
-  );
+		// <div className="body">
+		//   {/* <Events /> */}
+		//   <Homepage />
+		//   {/* <Sponsors /> */}
+		//   {/* <Workshop /> */}
+		// </div>
+	);
 }
 
 export default App;
