cc0be189575744802efd2cfab74709aff3b2e3d8
lohithsrk <srklohith05@gmail.com>
2023-02-21 08:16:37 +0530

    changes

--- src/axios/register.axios.js
+++ src/axios/register.axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 const base_url = "http://localhost:8080/api";
 
-export const register = (name, email, phone, college, year, event) =>
-	axios.post(`${base_url}/data`, {name, email, phone, college, year, event});
+export const register = (name, email, phone, college, dept, year, events) =>
+	axios.post(`${base_url}/data`, { name, email, phone, college, dept, year, events });
