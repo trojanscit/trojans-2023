609766f5c5bc7a8168aadf159a9d058815c7c9d4
Prasanna <prasannavelmurugan0200@gmail.com>
2023-03-04 21:06:39 +0530

    rv

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,25 +1,25 @@
-import React, { useState, useRef } from 'react';
-import '../styles/registration.css';
-import Navbar from './navbar/navbar';
-import Footer from './footer/footer';
-import { toast } from 'react-toastify';
-import { Link, useNavigate } from 'react-router-dom';
-import { register } from '../axios/register.axios';
-import { Helmet } from 'react-helmet';
+import React, {useState, useRef} from "react";
+import "../styles/registration.css";
+import Navbar from "./navbar/navbar";
+import Footer from "./footer/footer";
+import {toast} from "react-toastify";
+import {Link, useNavigate} from "react-router-dom";
+import {register} from "../axios/register.axios";
+import {Helmet} from "react-helmet";
 
 function Registration() {
-	const [techType, setTechEventType] = useState('Technical');
-	const [nonTechType, setNonTechEventType] = useState('Technical');
-	const [gamingType, setGamingEventType] = useState('Technical');
-	const [workshopType, setWorkshopEventType] = useState('Technical');
+	const [techType, setTechEventType] = useState("Technical");
+	const [nonTechType, setNonTechEventType] = useState("Technical");
+	const [gamingType, setGamingEventType] = useState("Technical");
+	const [workshopType, setWorkshopEventType] = useState("Technical");
 
-	const name = useRef('');
-	const email = useRef('');
-	const phone = useRef('');
-	const college = useRef('');
-	const year = useRef('');
+	const name = useRef("");
+	const email = useRef("");
+	const phone = useRef("");
+	const college = useRef("");
+	const year = useRef("");
 
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
@@ -29,10 +29,10 @@ function Registration() {
 			phone.current.value,
 			college.current.value,
 			year.current.value,
-			{ techType, nonTechType, gamingType, workshopType }
+			{techType, nonTechType, gamingType, workshopType}
 		);
-		await toast.success('Good job');
-		navigate("/")
+		// await toast.success("Good job");
+		navigate("/");
 	};
 
 	return (
@@ -41,61 +41,86 @@ function Registration() {
 			<Helmet>
 				<title>Registration | Trojans</title>
 			</Helmet>
-			<section className='sectionRegistration'>
-				<div class='container1'>
-					<div class='title'>
+			<section className="sectionRegistration">
+				<div class="container1">
+					<div class="title">
 						<h1>Registration</h1>
-						<Link to='/'>
+						<Link to="/">
 							<img
-								src='https://img.icons8.com/ios-filled/256/multiply.png'
-								alt='Cancel button'
+								src="https://img.icons8.com/ios-filled/256/multiply.png"
+								alt="Cancel button"
 							/>
 						</Link>
 					</div>
-					<form className='formRegistration' onSubmit={(e) => handleSubmit(e)}>
-						<div class='user__details'>
-							<div class='input__box'>
-								<span class='details'>Full Name</span>
+					<form
+						className="formRegistration"
+						onSubmit={(e) => handleSubmit(e)}
+					>
+						<div class="user__details">
+							<div class="input__box">
+								<span class="details">Full Name</span>
 								<input
-									type='text'
-									placeholder='E.g: John Smith'
+									type="text"
+									placeholder="E.g: John Smith"
 									required
 									ref={name}
 								/>
 							</div>
-							<div class='input__box'>
-								<span class='details'>Email</span>
+							<div class="input__box">
+								<span class="details">Email</span>
 								<input
-									type='email'
-									placeholder='johnsmith@hotmail.com'
+									type="email"
+									placeholder="johnsmith@hotmail.com"
 									required
 									ref={email}
 								/>
 							</div>
-							<div class='input__box'>
-								<span class='details'>Phone Number</span>
+							<div class="input__box">
+								<span class="details">Phone Number</span>
 								<input
-									type='tel'
+									type="tel"
 									// pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
-									placeholder='012-345-6789'
+									placeholder="012-345-6789"
 									required
 									ref={phone}
 								/>
 							</div>
-							<div class='input__box'>
-								<span class='details'>Year</span>
-								<input type='text' placeholder='1st year' required ref={year} />
+							<div class="input__box">
+								<span class="details">Year</span>
+								{/* <input
+									type="text"
+									placeholder="1st year"
+									required
+									ref={year}
+								/> */}
+								<select
+									name=""
+									id=""
+									// onChange={(e) => {
+									// 	setTechEventType(e.target.value);
+									// }}
+									ref={year}
+									defaultValue="Select"
+								>
+									<option value="Select">
+										Select any option
+									</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+								</select>
 							</div>
-							<div class='input__box'>
-								<span class='details'>College Name</span>
+							<div class="input__box">
+								<span class="details">College Name</span>
 								<input
-									type='text'
-									placeholder='Chennai Institute of Technology'
+									type="text"
+									placeholder="Chennai Institute of Technology"
 									required
 									ref={college}
 								/>
 							</div>
-							<div class='input__box'>
+							<div class="input__box">
 								{/* <span class="details">Event Type</span> */}
 								{/* <select
 								name=""
@@ -112,79 +137,107 @@ function Registration() {
 								<option value="gaming">Gaming</option>
 								<option value="workshop">Workshop</option>
 							</select> */}
-								<span class='details'>Technical</span>
+								<span class="details">Technical</span>
 								<select
-									name=''
-									id=''
+									name=""
+									id=""
 									onChange={(e) => {
 										setTechEventType(e.target.value);
 									}}
-									defaultValue='Select'
+									defaultValue="Select"
 								>
-									<option value='Select'>Select any option</option>
-									<option value='Coders Chemistry'>Coders Chemistry</option>
-									<option value='CLEVER BOT'>CLEVER BOT</option>
-									<option value='Rhythm Rivalry'>Rhythm Rivalry</option>
-									<option value='The Hunt For The Hidden'>
+									<option value="Select">
+										Select any option
+									</option>
+									<option value="Coders Chemistry">
+										Coders Chemistry
+									</option>
+									<option value="CLEVER BOT">
+										CLEVER BOT
+									</option>
+									<option value="Rhythm Rivalry">
+										Rhythm Rivalry
+									</option>
+									<option value="The Hunt For The Hidden">
 										The Hunt For The Hidden
 									</option>
-									<option value='Code Jigsaw'>Code Jigsaw</option>
-									<option value='FinalDraft'>FinalDraft</option>
+									<option value="Code Jigsaw">
+										Code Jigsaw
+									</option>
+									<option value="FinalDraft">
+										FinalDraft
+									</option>
 								</select>
 							</div>
-							<div class='input__box'>
-								<span class='details'>Non Technical</span>
+							<div class="input__box">
+								<span class="details">Non Technical</span>
 								<select
-									name=''
-									id=''
+									name=""
+									id=""
 									onChange={(e) => {
 										setNonTechEventType(e.target.value);
 									}}
-									defaultValue='Select'
+									defaultValue="Select"
 								>
-									<option value='Select'>Select any option</option>
-									<option value='SHUTTER STOP'>SHUTTER STOP</option>
-									<option value='TALENTRON'>TALENTRON</option>
-									<option value='SHIP WRECK'>SHIP WRECK</option>
-									<option value='GULLY CRICKET'>GULLY CRICKET</option>
-									<option value='COMIC QUIZ'>COMIC QUIZ</option>
-									<option value='SQUID GAME'>SQUID GAME</option>
-									<option value='OKAY FINE'>OKAY FINE</option>
+									<option value="Select">
+										Select any option
+									</option>
+									<option value="SHUTTER STOP">
+										SHUTTER STOP
+									</option>
+									<option value="TALENTRON">TALENTRON</option>
+									<option value="SHIP WRECK">
+										SHIP WRECK
+									</option>
+									<option value="GULLY CRICKET">
+										GULLY CRICKET
+									</option>
+									<option value="COMIC QUIZ">
+										COMIC QUIZ
+									</option>
+									<option value="SQUID GAME">
+										SQUID GAME
+									</option>
+									<option value="OKAY FINE">OKAY FINE</option>
 								</select>
 							</div>
-							<div class='input__box'>
-								<span class='details'>Gaming</span>
+							{/* <div class="input__box">
+								<span class="details">Gaming</span>
 								<select
-									name=''
-									id=''
+									name=""
+									id=""
 									onChange={(e) => {
 										setGamingEventType(e.target.value);
 									}}
 								>
-									<option value='technical'>Technical</option>
-									<option value='non-technical'>Non Technical</option>
-									<option value='gaming'>Gaming</option>
-									<option value='workshop'>Workshop</option>
+									<option value="technical">Technical</option>
+									<option value="non-technical">
+										Non Technical
+									</option>
+									<option value="gaming">Gaming</option>
+									<option value="workshop">Workshop</option>
 								</select>
 							</div>
-							<div class='input__box'>
-								<span class='details'>Workshop</span>
+							<div class="input__box">
+								<span class="details">Workshop</span>
 								<select
-									name=''
-									id=''
+									name=""
+									id=""
 									onChange={(e) => {
 										setWorkshopEventType(e.target.value);
 									}}
 								>
-									<option value='technical'>Technical</option>
-									<option value='non-technical'>Non Technical</option>
-									<option value='gaming'>Gaming</option>
-									<option value='workshop'>Workshop</option>
+									<option value="technical">Technical</option>
+									<option value="non-technical">
+										Non Technical
+									</option>
+									<option value="gaming">Gaming</option>
+									<option value="workshop">Workshop</option>
 								</select>
-							</div>
+							</div> */}
 						</div>
-						<div class='button'>
-							<input type='submit' value='Register' />
+						<div class="button">
+							<input type="submit" value="Register" />
 						</div>
 					</form>
 				</div>
--- src/axios/register.axios.js
+++ src/axios/register.axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const base_url = "http://localhost:8080/api";
-// const base_url = "https://trojans2023-server.onrender.com";
+// const base_url = "http://localhost:8080/api";
+const base_url = "https://trojans2023-server.onrender.com/api";
 
 export const register = (name, email, phone, college, year, events) =>
-	axios.post(`${base_url}/data`, { name, email, phone, college, year, events });
+	axios.post(`${base_url}/data`, {name, email, phone, college, year, events});
